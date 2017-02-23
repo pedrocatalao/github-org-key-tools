39eb0dc861eb9f1b26390beb8a2d97f2e5a45d00
Pedro Catalão <pedrocatalao@users.noreply.github.com>
2017-02-23 21:49:23 +0000

    Now supports parameters
    -u username
    -t token
    -o organization

--- get-all-keys.sh
+++ get-all-keys.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
-mylogin=pedrocatalao
-organization=kabisa
-mypassword=my_token_or_password
-members_url="https://api.github.com/orgs/$organization/members?per_page=100"
+me=`basename "$0"`
 
-for i in `curl -u $mylogin:$mypassword $members_url 2>&1 | grep login | sed 's/\"login"://g;s/[\", ]//g'` ; do
- key=`curl --silent -u $mylogin:$mypassword "https://github.com/$i.keys"` && echo -e "\n### $i ###\n\n$key\n" ;
+while [[ $# -gt 1 ]]
+do
+	key="$1"
+	case $key in
+	    -u|--user)
+	    user="$2"
+	    shift
+	    ;;
+	    -t|--token)
+	    token="$2"
+	    shift
+	    ;;
+	    -o|--org)
+	    organization="$2"
+	    shift
+	    ;;
+	    *)
+	    # unknown option
+	    ;;
+	esac
+	shift
+done
+
+if [ -z "$user" ] || [ -z "$token" ] || [ -z "$organization" ]; then
+	echo "Usage: $me -u username -t token -o organization"
+	exit 1
+fi
+
+base_url="https://api.github.com/orgs/$organization/members?per_page=100"
+
+for i in `curl -u $user:$token $base_url 2>&1 | grep login | sed 's/\"login"://g;s/[\", ]//g'` ; do
+	key=`curl --silent -u $user:$token "https://github.com/$i.keys"` && echo -e "\n### $i ###\n\n$key\n" ;
 done
